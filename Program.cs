4954546934a499a915f38a07674d66a550d9d1da
Nikita Mavrinsky <sharabarovnv@gmail.com>
2023-03-09 17:52:53 +0600

    done the second way

--- Program.cs
+++ Program.cs
@@ -3,11 +3,21 @@ Console.Write("Enter the array elements with spaces: ");
 string input = Console.ReadLine()!;
 Console.Write("Enter the number of symbols, that the elements you want to find, consist of: ");
 int num = int.Parse(Console.ReadLine()!);
+
 string[] array = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-Console.WriteLine($"[{string.Join(" ", SeveralSymbolElements(array, num))}]");
+Console.WriteLine($"[{string.Join(" ", SeveralSymbolElements1(array, num))}]");
+Console.WriteLine($"[{string.Join(" ", SeveralSymbolElements2(array, num))}]");
 
+string[] SeveralSymbolElements1(string[] inArray, int number){
+    string result = string.Empty;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if(inArray[i].Length <= number) result += inArray[i] + " ";
+    }
+    return result.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+}
 
-string[] SeveralSymbolElements(string[] inArray, int number){
+string[] SeveralSymbolElements2(string[] inArray, int number){
     int count = 0;
     for (int i = 0; i < inArray.Length; i++){
         if (inArray[i].Length <= number) count++;
