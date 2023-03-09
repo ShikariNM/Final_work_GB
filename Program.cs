3acc9e8c329e9d9200937f6bf5d59dfc402255c5
Nikita Mavrinsky <sharabarovnv@gmail.com>
2023-03-09 17:33:49 +0600

    done the first way

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Enter the array elements with spaces: ");
+string input = Console.ReadLine()!;
+Console.Write("Enter the number of symbols, that the elements you want to find, consist of: ");
+int num = int.Parse(Console.ReadLine()!);
+string[] array = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"[{string.Join(" ", SeveralSymbolElements(array, num))}]");
+
+
+string[] SeveralSymbolElements(string[] inArray, int number){
+    int count = 0;
+    for (int i = 0; i < inArray.Length; i++){
+        if (inArray[i].Length <= number) count++;
+    }
+    string[] result = new string[count];
+    for (int i = 0, j = 0; i < inArray.Length; i++){
+        if (inArray[i].Length <= number){
+            result[j] = inArray[i];
+            j++;
+        }
+    }
+    return result;
+}
